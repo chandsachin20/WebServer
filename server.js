ed74dd3b7a4847658257b97fa30fd65e3ff4b9d1
Sachin Chand <sachinchand20@gmail.com>
2019-02-16 12:53:35 +0530

    Added handle bars and views

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+
+app.set('view engine',hbs);
 
 //middleware ofr setting static directory
 app.use(express.static(__dirname + '/public'));
@@ -8,18 +11,27 @@ app.use(express.static(__dirname + '/public'));
 //http route handler
 app.get('/',(req,res)=>{
    // res.send('<h1>Hello Express!</h1>');
-    res.send({
+   /* res.send({
         name:'Sachin',
         like: [
             'biking',
             'coding'
         ]
-    })
+    })*/
+    res.render('index.hbs',{
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear() 
+    });
+
 });
 
 //about page route
 app.get('/about',(req,res) =>{
-    res.send('<h1>About Page</h1>');
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        currentYear:new Date().getFullYear(),
+        message:'This server is being developed by Sachin Chand.'
+    });
 })
 
 //route handler ofr json error page 
